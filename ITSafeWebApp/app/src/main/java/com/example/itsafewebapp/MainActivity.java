897ef872e8fedaa7d334eef05e0b0c09970a1455
Roman Zaikin <romans5427173@gmail.com>
2019-04-24 23:54:03 +0300

    first upload
    first upload

---
+++ ITSafeWebApp/app/src/main/java/com/example/itsafewebapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.itsafewebapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview =(WebView)findViewById(R.id.webView);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("https://challenges.itsafe.co.il/MOBILE/index.html");
+
+    }
+}
